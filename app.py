ad20967eec1a8b362524ea83f3d77e78111568bd
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2015-06-03 13:07:25 +0900

    move "feature_type" from json query to URL string.

--- app.py
+++ app.py
@@ -20,10 +20,6 @@ app.config.load_config("%s/myapp.conf" % app.config['root'])
 mongo_client = MongoClient(app.config['myapp.mongo_host'],int(app.config['myapp.mongo_port']))
 
 
-@app.route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
-
 @error('404')
 def error404(error):
 	return '{"status": "Error", "message":"No such algorithm"}'
@@ -35,8 +31,8 @@ def parse_params(params):
 	return hash
 
 # operation: add, clear, train, predict
-@app.route('/ml/<database>/<algorithm>/<operation>', ['GET', 'POST'])
-def route_machine_learning_basic(database, algorithm, operation):
+@app.route('/ml/<database>/<feature_type>/<algorithm>/<operation>', ['GET', 'POST'])
+def route_machine_learning_basic(database, feature_type, algorithm, operation):
 	print 'function: route'
 	params = parse_params(request.params)
 	if params.has_key('json_data'):
@@ -45,13 +41,13 @@ def route_machine_learning_basic(database, algorithm, operation):
 		json_data_s = "{}"
 	print "param: " + json_data_s
 	db = mongo_client[database]
-	result = my_classifier.route(db,json_data_s, operation, algorithm)
+	result = my_classifier.route(db, json_data_s, operation, feature_type, algorithm)
 	print result
 	return json.dumps(result,default=json_util.default)
 
 
 # algorithmの指定なくてもサンプルは追加可能
-@app.route('/ml/<database>/add')
+@app.route('/ml/<database>/<feature_type>/add')
 def get_add(database):
 	if request.params.__dict__.has_key('json_data'):
 		json_data_s = request.params.json_data
@@ -63,17 +59,28 @@ def get_add(database):
 	return json.dumps(result,default=json_util.default)
 
 
+@app.route('/ml/<database>/<feature_type>/clear_samples')
+def get_clearsamples(database):
+	if request.params.__dict__.has_key('json_data'):
+		json_data_s = request.params.json_data
+	else:
+		return my_classifier.error_json("parameter 'json_data' must be set.")
+	db = mongo_client[database]
+	result = my_classifier.route(db,json_data_s,'clear_samples')
+	print "result" + result
+	return json.dumps(result,default=json_util.default)
+
 
 
 
 #	one-out-leave
-@app.get('/ml/<database>/<algorithm>/one-out-leave/<data_id>')
+@app.get('/ml/<database>/<feature_type>/<algorithm>/one-out-leave/<data_id>')
 def get_one_out_leave(database,algorithm,data_id):
 	db = mongo_client[database]
 	pass
 
 # cross-validation
-@app.get('/ml/<database>/<algorithm>/cross-validation/<fold_num:int>')
+@app.get('/ml/<database>/<feature_type>/<algorithm>/cross-validation/<fold_num:int>')
 def get_cross_validation(database,algorithm,fold_num):
 	db = mongo_client[database]
 	pass
