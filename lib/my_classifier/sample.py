55c86f463ca7d4d99eecaa51ada4d594a319a21d
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2015-06-22 21:22:24 +0900

    accept dict -> sample transform.

--- lib/my_classifier/sample.py
+++ lib/my_classifier/sample.py
@@ -13,9 +13,14 @@ def ensure_list(elem):
             
 class Sample:            
         def __init__(self, json_data):
-		self.ft = json_data['feature']
+		if json_data.has_key('feature'):
+			self.ft = json_data['feature']
+		elif json_data.has_key('ft'):
+			self.ft = json_data['ft']
 		if json_data.has_key('id'):
 			self._id = json_data['id']
+		elif json_data.has_key('_id'):
+      			self._id = json_data['_id']
 		if json_data.has_key('ground_truth'):
 			self.ground_truth = json_data['ground_truth']
 		if json_data.has_key('group'):
@@ -28,7 +33,7 @@ class Sample:
 		self.weight = 1.0
 		if json_data.has_key('weight'):
 			self.weight = json_data['weight']
-
+      
 if __name__ == '__main__':
 	json_data = json.loads('{"feature":[0,0,0,0]}')
 												 #"{'feature':[1.2,3,0.2,-1], 'id':'2014RC01_S020::frame0000001::blob001::0003','class':'tomato'}")
